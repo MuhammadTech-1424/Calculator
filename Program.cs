2591b9fd5e6c118f5c91b3efa242d64df2678b30
Muhammadjon <muhammadyashnar2003@gmail.com>
2025-02-18 19:54:35 +0500

    Taqqoslash operatorlari darsi

--- Program.cs
+++ Program.cs
@@ -2,12 +2,19 @@
 
 Console.WriteLine("Bu KALKULYATOR dasturi");
 
+// Input operatori
 Console.Write("1-sonni kiriting: ");
 double son1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("2-sonni kiriting: ");
 double son2 = Convert.ToDouble(Console.ReadLine());
 
+// Matematik operatorlar
 Console.WriteLine($"Sonlar yig'indisi {son1 + son2}.");
 Console.WriteLine($"Sonlar ayirmasi {son1 - son2}.");
 Console.WriteLine($"Sonlar ko'paytmasi {son1 * son2}.");
 Console.WriteLine($"Sonlar bo'linmasi {son1 / son2}, qoldiq {son1 % son2}.");
+
+// Taqqoslash operatorlari
+Console.WriteLine($"1-son 2-sidan kattami? {son1 > son2}");
+Console.WriteLine($"1-son 2-sidan kichikmi? {son1 < son2}");
+Console.WriteLine($"Sonlar tengmi? {son1 == son2}");
